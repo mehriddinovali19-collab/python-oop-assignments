589420cd49be11fafc33a39c99896d7d338022ef
ali mekhridinov <mehriddinovali19@gmail.com>
2026-05-06 09:32:59 +0500

    task06 done

---
+++ task06.py
@@ -0,0 +1,16 @@
+class Student:
+
+    def __init__(self, name: str, age: int, grade: int):
+        self.n = name
+        self.a = age
+        self.g = grade 
+
+
+    def info(self):
+        print(f"information about student: Name: {self.n}, Age: {self.a}, Grade: {self.g}")
+
+student01 = Student("ali", 15, 9)
+student02 = Student("vali", 18, 11)
+student03 = Student("sami", 16, 10)
+
+student01.info()
